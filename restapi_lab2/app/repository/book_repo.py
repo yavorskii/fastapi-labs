6cd93b0e39c45a69ccf2df0cdbb64216e054d4a5
Vladyslav Yavorskyi <vladyavorskii295@gmail.com>
2026-05-03 23:15:59 +0300

    implement filtering and pagination in repository sql queries

--- restapi_lab2/app/repository/book_repo.py
+++ restapi_lab2/app/repository/book_repo.py
@@ -2,8 +2,16 @@ from sqlalchemy.orm import Session
 from app.models.book_model import BookModel
 
 class BookRepository:
-    async def get_all(self, db: Session, limit: int, offset: int):
-        return db.query(BookModel).offset(offset).limit(limit).all()
+    async def get_all(self, db: Session, limit: int, offset: int, status=None, author=None):
+        query = db.query(BookModel)
+        
+        
+        if status:
+            query = query.filter(BookModel.status == status)
+        if author:
+            query = query.filter(BookModel.author.ilike(f"%{author}%"))
+        
+        return query.offset(offset).limit(limit).all()
 
     async def add(self, db: Session, book_data: dict):
         db_book = BookModel(**book_data)
@@ -11,3 +19,14 @@ class BookRepository:
         db.commit()
         db.refresh(db_book)
         return db_book
+
+    async def get_by_id(self, db: Session, book_id):
+        return db.query(BookModel).filter(BookModel.id == book_id).first()
+
+    async def delete(self, db: Session, book_id):
+        book = db.query(BookModel).filter(BookModel.id == book_id).first()
+        if book:
+            db.delete(book)
+            db.commit()
+            return True
+        return False

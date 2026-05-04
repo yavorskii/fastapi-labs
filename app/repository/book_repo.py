04e6d543a64b25bc91195fca5e45374656f18861
Vladyslav Yavorskyi <vladyavorskii295@gmail.com>
2026-05-04 23:32:25 +0300

    implement cursor pagination logic in BookRepository

--- app/repository/book_repo.py
+++ app/repository/book_repo.py
@@ -1,17 +1,25 @@
 from sqlalchemy.orm import Session
 from app.models.book_model import BookModel
+from uuid import UUID
+from typing import Optional
 
 class BookRepository:
-    async def get_all(self, db: Session, limit: int, offset: int, status=None, author=None):
-        query = db.query(BookModel)
-        
-        
+    async def get_all(self, db: Session, size: int, cursor: Optional[UUID], status=None, author=None):
+        query = db.query(BookModel).order_by(BookModel.id)
+
         if status:
             query = query.filter(BookModel.status == status)
         if author:
             query = query.filter(BookModel.author.ilike(f"%{author}%"))
         
-        return query.offset(offset).limit(limit).all()
+        if cursor:
+            query = query.filter(BookModel.id > cursor)
+        
+        books = query.limit(size).all()
+        
+        next_cursor = books[-1].id if len(books) == size else None
+        
+        return books, next_cursor
 
     async def add(self, db: Session, book_data: dict):
         db_book = BookModel(**book_data)
@@ -24,7 +32,7 @@ class BookRepository:
         return db.query(BookModel).filter(BookModel.id == book_id).first()
 
     async def delete(self, db: Session, book_id):
-        book = db.query(BookModel).filter(BookModel.id == book_id).first()
+        book = self.get_by_id(db, book_id) 
         if book:
             db.delete(book)
             db.commit()

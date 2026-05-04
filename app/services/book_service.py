8b00f95f4dcf6f4eccdb62c75108aec3d622fa56
Vladyslav Yavorskyi <vladyavorskii295@gmail.com>
2026-05-04 23:45:38 +0300

    refactor: update BookService

--- app/services/book_service.py
+++ app/services/book_service.py
@@ -1,17 +1,29 @@
-from app.repository.book_repo import BookRepository
-from sqlalchemy.orm import Session
-from uuid import UUID
+from app.repository.mongo_repo import MongoBookRepository
+from motor.motor_asyncio import AsyncIOMotorDatabase
 from typing import Optional
 
 class BookService:
     def __init__(self):
-        self.repo = BookRepository()
+        self.repo = MongoBookRepository
 
-    
-    async def get_books(self, db: Session, size: int, cursor: Optional[UUID], status=None, author=None):
-        books, next_cursor = await self.repo.get_all(db, size, cursor, status, author)
+    async def get_books(self, db: AsyncIOMotorDatabase, limit: int, offset: int, status: Optional[str] = None, author: Optional[str] = None):
+        repo_instance = self.repo(db)
+        
+        books = await repo_instance.get_all(db, limit, offset, status, author)
+        
+        total = await db.books.count_documents(self._build_query(status, author))
         
         return {
             "items": books,
-            "next_cursor": next_cursor
+            "total": total,
+            "limit": limit,
+            "offset": offset
         }
+
+    def _build_query(self, status: Optional[str] = None, author: Optional[str] = None):
+        query = {}
+        if status:
+            query["status"] = status
+        if author:
+            query["author"] = {"$regex": author, "$options": "i"}
+        return query

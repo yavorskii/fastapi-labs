4a130fa114cbf49f5d71371933da28c0ce4cba8c
Vladyslav Yavorskyi <vladyavorskii295@gmail.com>
2026-05-04 23:27:46 +0300

    update router and service to support cursor pagination

--- app/services/book_service.py
+++ app/services/book_service.py
@@ -1,9 +1,17 @@
 from app.repository.book_repo import BookRepository
 from sqlalchemy.orm import Session
+from uuid import UUID
+from typing import Optional
 
 class BookService:
     def __init__(self):
         self.repo = BookRepository()
 
-    async def get_books(self, db: Session, limit: int, offset: int, status=None, author=None):
-        return await self.repo.get_all(db, limit, offset, status, author)
+    
+    async def get_books(self, db: Session, size: int, cursor: Optional[UUID], status=None, author=None):
+        books, next_cursor = await self.repo.get_all(db, size, cursor, status, author)
+        
+        return {
+            "items": books,
+            "next_cursor": next_cursor
+        }

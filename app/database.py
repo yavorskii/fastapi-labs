b615691239afcfd6e9e58d25f03033e3e7584704
Vladyslav Yavorskyi <vladyavorskii295@gmail.com>
2026-05-04 23:42:36 +0300

    refactor: change database connection to MongoDB motor

--- app/database.py
+++ app/database.py
@@ -1,17 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/library")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://user:password@db:27017")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+client = AsyncIOMotorClient(MONGO_URL)
+db = client.library_db
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def get_db():
+    yield db

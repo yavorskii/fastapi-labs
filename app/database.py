edbe30579a1085f82f7877f21a7dd61c99e3ac42
Vladyslav Yavorskyi <vladyavorskii295@gmail.com>
2026-05-06 20:40:23 +0300

    make repository asynchronous and add user management methods

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://user:password@db:27017")
 
-client = MongoClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client.library_db
+
+
+async def get_db():
+    return db
--- app/repository/mongo_repo.py
+++ app/repository/mongo_repo.py
@@ -3,9 +3,11 @@ from typing import List, Optional
 
 class MongoBookRepository:
     def __init__(self, db):
+        self.db = db
         self.collection = db.books
+        self.users_collection = db.users 
 
-    def get_all(self, db, limit: int, offset: int, status: Optional[str] = None, author: Optional[str] = None):
+    async def get_all(self, db, limit: int, offset: int, status: Optional[str] = None, author: Optional[str] = None):
         query = {}
         if status:
             query["status"] = status
@@ -13,27 +15,34 @@ class MongoBookRepository:
             query["author"] = {"$regex": author, "$options": "i"}
         
         cursor = self.collection.find(query).skip(offset).limit(limit)
-        books = list(cursor)
+        books = await cursor.to_list(length=limit)
         
         for book in books:
             book["id"] = str(book.pop("_id"))
         return books
 
-    def add(self, db, book_data: dict):
-        result = self.collection.insert_one(book_data)
+    async def add(self, db, book_data: dict):
+        result = await self.collection.insert_one(book_data)
         book_data["id"] = str(result.inserted_id)
         return book_data
 
-    def get_by_id(self, db, book_id: str):
+    async def get_by_id(self, db, book_id: str):
         if not ObjectId.is_valid(book_id):
             return None
-        book = self.collection.find_one({"_id": ObjectId(book_id)})
+        book = await self.collection.find_one({"_id": ObjectId(book_id)})
         if book:
             book["id"] = str(book.pop("_id"))
         return book
 
-    def delete(self, db, book_id: str):
+    async def delete(self, db, book_id: str):
         if not ObjectId.is_valid(book_id):
             return False
-        result = self.collection.delete_one({"_id": ObjectId(book_id)})
+        result = await self.collection.delete_one({"_id": ObjectId(book_id)})
         return result.deleted_count > 0
+
+    async def get_user(self, username: str):
+        return await self.users_collection.find_one({"username": username})
+
+    async def create_user(self, user_data: dict):
+        await self.users_collection.insert_one(user_data)
+        return user_data

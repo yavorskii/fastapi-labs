d50db8edcda763b3232d3279f02975794635f724
Vladyslav Yavorskyi <vladyavorskii295@gmail.com>
2026-05-07 15:47:13 +0300

    add locust load testing script

---
+++ locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+
+class LibraryUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def get_books(self):
+        self.client.get("/books")
+
+    @task(3)
+    def get_root(self):
+        self.client.get("/")
